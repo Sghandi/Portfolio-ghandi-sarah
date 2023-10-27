4397de8b022eda50da8f3dcd06e426da788f8cc7
Sarah Ghandi <ghandi.sarah33@gmail.com>
2023-10-27 11:38:42 +0200

    init

---
+++ src/components/Header/header.js
@@ -0,0 +1,16 @@
+function Header() {
+    return (
+        
+        <nav className="header-nav">
+        <div className="logo-container">
+            <img src="" alt="Logo" />
+        </div>
+        <div className="links-container">
+            <Link to="/">Accueil</Link>
+            <Link to="/About">A Propos</Link>
+        </div>
+    </nav>
+    )
+}
+
+export default Header
